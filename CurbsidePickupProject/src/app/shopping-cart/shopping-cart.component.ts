3ee6d5932defd75150ce639e7686cac1f72eb3e1
Reece <reece.jm61@gmail.com>
2021-06-30 23:39:48 -0500

    updated shopping cart css

--- CurbsidePickupProject/src/app/shopping-cart/shopping-cart.component.ts
+++ CurbsidePickupProject/src/app/shopping-cart/shopping-cart.component.ts
@@ -12,4 +12,51 @@ export class ShoppingCartComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // $(document).ready(function () {
+  //   updateAmounts();
+  //   $('.qty, .price').on('keyup keypress blur change',
+  //   function(e) {
+  //     updateAmounts();
+  //   });
+    
+  // });
+
+  // function updateAmounts(): void {
+  //   var sum: number = 0.0;
+  //   $('#myTable > tbody > tr').each(function() {
+  //     var qty: number = $(this).find('.qty').val();
+  //     var price: number = $(this).find('.price').val();
+  //     var amount: number = (qty * price)
+  //     sum += amount;
+  //     $(this).find('.amount').text('' + amount);
+  //   });
+  //   $('.total').text(sum);
+
+
+// for quantity ++ or --
+
+// var incementQty;
+// var decrementQty;
+// var plusBtn = $(".cart-qty-plus");
+// var minusBtn = $("cart-qty-minus");
+// var incrementQty = plusBtn.click(function() {
+//   var $n = $(this)
+//   .parent(".button-container")
+//   .find(".qty");
+//   $n.val(Number($n.val())+1);
+//   updateAmounts();
+// });
+
+// var decrementQty = minusBtn.click(function() {
+//   var $n = $(this)
+//   .parent(".button-container")
+//   .find(".qty");
+//   var QtyVal = Number($n.val());
+//   if(QtyVal > 0) {
+//     $n.val(QtyVal - 1);
+//   }
+//   updateAmounts();
+// });
+  
+
 }
