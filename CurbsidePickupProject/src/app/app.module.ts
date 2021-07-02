a176b1919696e6e36eea1f20d1b2bf63639660d3
Reece <reece.jm61@gmail.com>
2021-07-02 00:54:39 -0500

    removed search my store component

--- CurbsidePickupProject/src/app/app.module.ts
+++ CurbsidePickupProject/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { SearchMyStoreComponent } from './search-my-store/search-my-store.component';
 import { ProductComponent } from './product/product.component';
 import { ProductInfoService } from './product/product-info.service';
 import { InventoryComponent } from './inventory/inventory.component';
@@ -12,7 +11,6 @@ import { InventoryComponent } from './inventory/inventory.component';
   declarations: [
     AppComponent,
     ShoppingCartComponent,
-    SearchMyStoreComponent,
     ProductComponent,
     InventoryComponent
   ],
